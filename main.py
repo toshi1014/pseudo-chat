0cddec6e89bb2467c1715b7ef80ad1477eb3607e
toshi1014 <toshihide1014go@gmail.com>
2021-07-30 16:14:58 +0900

    add: decode data from script.ts at main.py

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ hexadecimal_dict = {
 }
 
 
+def decode_uft8(byte_list):
+    str_hex = ""
+
+    for byte in byte_list.split(","):
+        str_hex_tmp = str(hex(int(byte)))[2:]        ## e.g. 228 => e4
+        str_hex += str_hex_tmp
+
+    byte_code = bytes.fromhex(str_hex)
+    return byte_code.decode("UTF-8")
+
+
 def byte2int(b1, b2):
     int_list = []
     for b in [b1, b2]:
@@ -22,8 +33,8 @@ def byte2int(b1, b2):
     return int_list[0]*16 + int_list[1]
 
 
-## make UTF-8 into int
-def encode_en_ja(string):
+## make UTF-8 into int      e.g. \0e4 => 228
+def encode_utf8(string):
     byte_code = str(string.encode("UTF-8"))[2:-1]        ## [2:-1] remove b"..."
 
     encoded_list = []
@@ -34,12 +45,12 @@ def encode_en_ja(string):
             continue
 
         if (len(byte_code)-i) > 1:      ## avoid out of range at 38 below
-            ## if not en
+            ## if not alphabet
             if (b == "\\") & (byte_code[i+1] == "x"):       ## e.g. \x93
                 encoded_list.append(byte2int(byte_code[i+2], byte_code[i+3]))
                 skip = 3        ## e.g. skip x,9,3
 
-            ## if en
+            ## if alphabet
             else:
                 encoded_list.append(ord(b))
 
@@ -47,8 +58,10 @@ def encode_en_ja(string):
 
 
 if __name__ == '__main__':
-    opponent = sys.stdin.readline().rstrip()
-    sender = sys.stdin.readline().rstrip()
+    encoded_opponent = sys.stdin.readline().rstrip()
+    encoded_sender = sys.stdin.readline().rstrip()
+    opponent = decode_uft8(encoded_opponent)
+    sender = decode_uft8(encoded_sender)
 
     message = generate_message.GenerateMessage(opponent).get_generated_message(sender)
-    print(",".join(list(map(lambda x: str(x), encode_en_ja(message)))))      ## make string: "," as delimiter
+    print(",".join(list(map(lambda x: str(x), encode_utf8(message)))))      ## make string: "," as delimiter
