22b4d2aabb6644871bfe02ab9124ae43cc15be7c
toshi1014 <toshihide1014go@gmail.com>
2021-08-01 00:45:43 +0900

    add: send generated message

--- src/message_client.py
+++ src/message_client.py
@@ -1,10 +1,16 @@
 from web_socket.client import WebsocketClient
+from core import *
 
 class MessageClient(WebsocketClient):
-    def __init__(self, address):
-        super().__init__(address)
+    def __init__(self, address, opponent_name_):
+        self.opponent_name = opponent_name_
+        super(MessageClient, self).__init__(address)
 
-    def on_message(self, ws, message):
-        ## if message came from js
-        if message[:4] == "[js]":
-            self.ws.send("[py]" + message[4:] + "!!!")
+    def on_message(self, ws, message_in):
+        ## if incoming_message came from js
+        if message_in[:4] == "[js]":
+            if message_in[:8] == "[js]send":
+                message_out = generate_message.GenerateMessage(self.opponent_name).get_generated_message("me")
+            elif message_in[:11] == "[js]receive":
+                message_out = generate_message.GenerateMessage(self.opponent_name).get_generated_message(self.opponent_name)
+            self.ws.send("[py]" + message_out)
