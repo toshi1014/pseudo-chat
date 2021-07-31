371db56a6d37ba42db8af87274224c34b88d5431
toshi1014 <toshihide1014go@gmail.com>
2021-07-31 22:15:12 +0900

    add: config.txt

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import glob, sys
 from core import *
-from handle_character_code import *
+from web_socket.server import WebsocketServerDerived
+from message_client import MessageClient
+import threading
 
+PORT = 5555
+HOST = "localhost"
+ADDRESS = f"ws://{HOST}:{PORT}"
 
 if __name__ == '__main__':
-    encoded_opponent = sys.stdin.readline().rstrip()
-    encoded_sender = sys.stdin.readline().rstrip()
-    opponent = decode_uft8(encoded_opponent)
-    sender = decode_uft8(encoded_sender)
-
-    message = generate_message.GenerateMessage(opponent).get_generated_message(sender)
-    print(",".join(list(map(lambda x: str(x), encode_utf8(message)))))      ## make string: "," as delimiter
+    server = threading.Thread(target=WebsocketServerDerived, args=(PORT, HOST))
+    server.start()
+    print("server launched successfully")
+    client = threading.Thread(target=MessageClient, args=([ADDRESS]))
+    client.start()
+    print("client connected successfully")
