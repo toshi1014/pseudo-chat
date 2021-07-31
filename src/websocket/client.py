ff294f89274c8b6a97517c03973725628a8eba4d
toshi1014 <toshihide1014go@gmail.com>
2021-07-31 13:01:51 +0900

    add: src/websocket

---
+++ src/websocket/client.py
@@ -0,0 +1,26 @@
+import websocket
+import _thread as thread
+import time
+
+
+class WebsocketClient():
+    def __init__(self, address):
+        self.ws = websocket.WebSocketApp(address,
+            on_message=lambda ws, msg: self.on_message(ws, msg),
+            on_close=lambda ws, msg: self.on_open(ws)
+        )
+        self.ws.on_open = lambda ws: self.on_open(ws)
+
+    def on_message(self, ws, message):
+        ## if message came from js
+        if message[:4] == "[js]":
+            self.ws.send("[py]" + message[4:] + "!!!")
+
+    def on_open(self, ws):
+        self.ws.send("[py] connected successfully")
+
+    def run_forever(self):
+        self.ws.run_forever()
+
+ADDRESS = "ws://localhost:5555"
+WebsocketClient(ADDRESS).run_forever()
---
+++ src/websocket/server.py
@@ -0,0 +1,25 @@
+from websocket_server import WebsocketServer
+
+class WebsocketServerDerived(WebsocketServer):
+    def __init__(self, port, host):
+        super().__init__(port, host=host)
+        self.run()
+
+    def new_client(self, client, server):
+        self.send_message_to_all("[server] new client has joined")
+
+    def client_left(self, client, server):
+        pass
+
+    def message_received(self, client, server, message):
+        self.send_message_to_all(message)
+
+    def run(self):
+        self.set_fn_new_client(self.new_client)
+        self.set_fn_client_left(self.client_left)
+        self.set_fn_message_received(self.message_received)
+        self.run_forever()
+
+PORT = 5555
+HOST = "localhost"
+WebsocketServerDerived(PORT, HOST)
