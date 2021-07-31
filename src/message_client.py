3a4b4e8a5d9bf33d64fabfff4521f565e74725f7
toshi1014 <toshihide1014go@gmail.com>
2021-08-01 00:49:20 +0900

    change: two Parent class for MessageClient

--- src/message_client.py
+++ src/message_client.py
@@ -1,16 +1,17 @@
 from web_socket.client import WebsocketClient
 from core import *
 
-class MessageClient(WebsocketClient):
+class MessageClient(generate_message.GenerateMessage, WebsocketClient):
     def __init__(self, address, opponent_name_):
         self.opponent_name = opponent_name_
-        super(MessageClient, self).__init__(address)
+        super(MessageClient, self).__init__(self.opponent_name)
+        super(generate_message.GenerateMessage, self).__init__(address)
 
     def on_message(self, ws, message_in):
         ## if incoming_message came from js
         if message_in[:4] == "[js]":
             if message_in[:8] == "[js]send":
-                message_out = generate_message.GenerateMessage(self.opponent_name).get_generated_message("me")
+                message_out = self.get_generated_message("me")
             elif message_in[:11] == "[js]receive":
-                message_out = generate_message.GenerateMessage(self.opponent_name).get_generated_message(self.opponent_name)
+                message_out = self.get_generated_message(self.opponent_name)
             self.ws.send("[py]" + message_out)
