1e7ff51569ea06f9a6541addc982f25ee564991f
toshi1014 <toshihide1014go@gmail.com>
2021-08-01 11:51:38 +0900

    change: log

--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ import threading
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 ADDRESS = f"ws://{HOST}:{PORT}"
+OPPONENT_NAME = sys.argv[3]
 
 if __name__ == '__main__':
     server = threading.Thread(target=WebsocketServerDerived, args=(PORT, HOST))
     server.start()
-    print("server launched successfully")
-    client = threading.Thread(target=MessageClient, args=([ADDRESS]))
+    print("server launching...")
+    client = threading.Thread(target=MessageClient, args=(ADDRESS, OPPONENT_NAME))
     client.start()
-    print("client connected successfully")
+    print("client connecting...")
