52b1c594f7cc087daba8d84f3d1ba203167a004d
toshi1014 <toshihide1014go@gmail.com>
2021-08-01 12:12:18 +0900

    wip

--- run.sh
+++ run.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
 
-set -e
+# set -e
+#
+# usage(){
+#     echo -e "" >&2
+#     echo -e "Usage: bash run.sh [options] <opponent's name>" >&2
+#     echo >&2
+#     echo "Options: " >&2
+#     echo -e "\t-r\trefresh all caches" >&2
+#     echo -e "\t-n\tnot check update in talk_histories/" >&2
+#
+#     exit 1
+# }
+#
+# if [ $# -gt 2 ] || [ $# -lt 1  ]; then
+#     usage
+# else
+#     while getopts rn OPT; do
+#         case $OPT in
+#             "r" ) REFRESH=True;;
+#             "n" ) NO_CHECK=True;;
+#             * ) usage;;
+#         esac
+#     done
+#     shift $(($OPTIND - 1))
+#
+#     if [ $# -ne 1 ]; then
+#         usage
+#     else
+#         OPPONENT_NAME=$1
+#     fi
+# fi
+#
+# if [ "$REFRESH" = "True" ]; then
+#     if ! [ ${#a[@]} -eq 0 ]; then
+#         for file in cache/*; do
+#             rm "$file"
+#         done
+#     fi
+# fi
+#
+# if ! [ "$NO_CHECK" = "True" ]; then
+#     python3 src/utils/handle_cache.py
+# fi
+#
 
-usage(){
-    echo -e "" >&2
-    echo -e "Usage: bash run.sh [options] <opponent's name>" >&2
-    echo >&2
-    echo "Options: " >&2
-    echo -e "\t-r\trefresh all caches" >&2
-    echo -e "\t-n\tnot check update in talk_histories/" >&2
 
-    exit 1
-}
-
-if [ $# -gt 2 ] || [ $# -lt 1  ]; then
-    usage
-else
-    while getopts rn OPT; do
-        case $OPT in
-            "r" ) REFRESH=True;;
-            "n" ) NO_CHECK=True;;
-            * ) usage;;
-        esac
-    done
-    shift $(($OPTIND - 1))
-
-    if [ $# -ne 1 ]; then
-        usage
-    else
-        OPPONENT_NAME=$1
-    fi
-fi
-
-if [ "$REFRESH" = "True" ]; then
-    if ! [ ${#a[@]} -eq 0 ]; then
-        for file in cache/*; do
-            rm "$file"
-        done
-    fi
-fi
-
-if ! [ "$NO_CHECK" = "True" ]; then
-    python3 src/utils/handle_cache.py
-fi
-
-
+# read & set config
 CONFIG=`cat config.txt`
 eval $CONFIG
 CODE="
@@ -55,5 +57,11 @@ const url = 'ws://${HOST}:${PORT}';
 sed -i "$ d" templates/index.js
 echo $CODE >> templates/index.js
 
-source $VENV_DIR/Scripts/activate
-python3 src/main.py $HOST $PORT
+#
+# source $VENV_DIR/bin/activate
+# echo $HOST $PORT $OPPONENT_NAME
+# python3 src/main.py $HOST $PORT $OPPONENT_NAME
+
+source env1/bin/activate
+OPPONENT_NAME=$1     #  debug
+python3 src/main.py localhost 5555 $OPPONENT_NAME
